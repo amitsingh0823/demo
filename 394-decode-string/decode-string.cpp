0be6fab0a1185d3dfd91f877e43a474dbce7a441
Amit Singh <amit.singh0823@gmail.com>
2026-03-13 00:31:54 +0530

    Time: 2 ms (23.64%) | Memory: 10.2 MB (7.72%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-
-    string solve(string &s, int &i, int num, string result) {
+    string solve(string& s, int& i, int num, string result) {
 
         if (i >= s.size() || s[i] == ']')
             return result;
 
         if (isdigit(s[i])) {
             num = num * 10 + (s[i] - '0');
-            return solve(s,i+=1, num, result);
+            return solve(s, i += 1, num, result);
         }
 
         if (s[i] == '[') {
-            string sub = solve(s, i+=1, 0, "");
+            string sub = solve(s, i += 1, 0, "");
 
-            // i++; // skip ']'
+            while (num--)result += sub;
 
-            while (num--)
-                result += sub;
-
-            return solve(s, i+=1, 0, result);
+            return solve(s, i += 1, 0, result);
         }
 
         result += s[i];
-        // i++;
 
-        return solve(s, i+=1, num, result);
+        return solve(s, i += 1, num, result);
     }
 
     string decodeString(string s) {
