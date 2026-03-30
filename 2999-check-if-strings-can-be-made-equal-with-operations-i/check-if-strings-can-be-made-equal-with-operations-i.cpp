49797812edf0ca028b9d7bb0733420c808c05914
Amit Singh <amit.singh0823@gmail.com>
2026-03-30 22:44:48 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (14.10%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        for(int i=0;i< 2;i++){
-            if(s1[i] == s2[i] || s1[i] == s2[i+2] ){
-                if(s1[i] == s2[i]) continue;
-                else {
-                    swap(s2[i],s2[i+2]);
-                }
-            }
-            else{
-                return false;
-            }
+        int freq[52] = {};
+
+        for (int i = 0; i < s1.length(); i++) {
+            int off = (i & 1) * 26;
+            freq[s1[i] - 'a' + off]++;
+            freq[s2[i] - 'a' + off]--;
         }
-        
-        if(s1[3] == s2[3] && s1[2] == s2[2])
-            return true;
-        return false;
+
+        for (int i = 0; i < 52; i++)
+            if (freq[i] != 0) return false;
+
+        return true;
     }
 };
